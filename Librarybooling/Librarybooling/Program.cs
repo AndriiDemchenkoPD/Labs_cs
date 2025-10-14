ab57784bf01c512781a46677d1eec86fc9e52686
AndriiDemchenkoPD <st9212850@stud.duikt.edu.ua>
2025-10-14 20:21:25 +0300

    first commit

---
+++ Librarybooling/Librarybooling/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Librarybooling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+
+            library.AddUser(library.CreateUser());
+            library.AddBook(library.CreateBook());
+
+            Reservation reservation = library.CreateReservation();
+            if (reservation == null)
+                return;
+
+            library.AddReservation(reservation);
+
+            library.ShowInfo();
+
+        }
+    }
+}
---
+++ Train/Train/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Train
+{
+    class Student
+    {
+        public Guid id;
+        public string firstName;
+        public string lastName;
+        public string middleName;
+        public int age;
+        public string group;
+
+    }
+    internal class Program
+    {
+        static Student GetStudent()
+        {
+            Student student = new Student();
+            student.id = Guid.NewGuid();
+            student.firstName = "Andrii";
+            student.lastName = "Demchenko";
+            student.middleName = "My";
+            student.age = 18;
+            student.group = "PD-12";
+
+            return student;
+        }
+
+        static void Print(Student student)
+        {
+            Console.WriteLine(student.id);
+            Console.WriteLine(student.firstName);
+            Console.WriteLine(student.lastName);
+            Console.WriteLine(student.middleName);
+            Console.WriteLine(student.age);
+            Console.WriteLine(student.group);
+        }
+ 
+       
+        static void Main(string[] args)
+        {
+            var firstStudent = GetStudent();
+            Print(firstStudent);
+        }
+    }
+}
